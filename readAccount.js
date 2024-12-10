2c4804c7cddd9e64f69c6d660666d222fd0063f1
Iván Mañús Murcia <ivanmanyus@bit2me.com>
2024-12-10 12:54:36 +0100

    Add identity information

--- readAccount.js
+++ readAccount.js
@@ -8,17 +8,30 @@ const axios  = require('axios');
 const { getAuthHeaders } = require('./bit2me_logic/utils');
 
 const PATH = process.env.END_ACCOUNT;
+const ID_PATH = process.env.END_ID_ACCOUNT;
 
 const args = process.argv.slice(2);
 const SUBACCOUNT = args[0]
 
 const readAccount = async () => {
     try {
-        const response = await axios.get(
-            `${process.env.SERVER}${PATH}`,
-            getAuthHeaders(PATH, SUBACCOUNT)
-        );
-        console.log(response.data);
+        const [user, identity] = await Promise.all([
+            axios.get(
+                `${process.env.SERVER}${PATH}`,
+                getAuthHeaders(PATH, SUBACCOUNT)
+            ),
+            axios.get(
+                `${process.env.SERVER}${ID_PATH}`,
+                getAuthHeaders(ID_PATH, SUBACCOUNT)
+            )
+        ]);
+
+        const response = {
+            ...user.data,
+            ...identity.data
+        };
+
+        console.log(response)
     }
     catch(e) {
         console.error(e.response.data)
